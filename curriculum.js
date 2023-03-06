d9476c4f4d9bd486c3d1f6e0c048a5a08be7e83c
marianadiaz5588 <126740994+marianadiaz5588@users.noreply.github.com>
2023-03-06 12:45:28 -0300

    initial commit

---
+++ curriculum.js
@@ -0,0 +1,22 @@
+
+document.getElementById('boton').onclick=function(){
+    console.log("Info de referencia");
+    document.getElementById("empresa").innerHTML="Roberto Martin<br>Gerencia- Punto Verde<br> Telefono: 264-4938980"
+    document.body.style.backgroundColor='#0000FF'
+}
+document.getElementById('boton1').onclick=function(){
+    console.log("Info de referencia");
+    document.getElementById("empresa").innerHTML="Multimedia Market- Punto Verde"
+    document.body.style.backgroundColor='#ff81de'}
+
+
+document.getElementById('boton2').onclick=function(){
+    console.log("Info de referencia");
+    document.getElementById("empresa2").innerHTML="Martina Diaz <br> R.R.H.H.- Logroño <br> Telefono: 264-56738735"
+    document.body.style.backgroundColor='#0000FF'
+}
+
+document.getElementById('boton3').onclick=function(){
+    console.log("Info de referencia");
+    document.getElementById("empresa2").innerHTML="Conservas Cidacos-Logroño"
+    document.body.style.backgroundColor='#ff81de'}
